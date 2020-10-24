a7d7bf1de7fec0125c7c1de2c9d1b4f91342e593
Lucian-CristianToma <vamix2012@gmail.com>
2020-10-25 00:02:51 +0200

    created layout and colapse extension

--- script.js
+++ script.js
@@ -1,21 +1,75 @@
-let itemsList = ['Mayan Avocado Salad', 'Beef Burrito', 'Pulled Pork Burrito Bowl'];
+function toggleButtons(id){
+if(id == 1){
+    document.getElementById('sectionToBeToggled1').classList.toggle('hide-extension')
+}
+if(id == 2){
+    document.getElementById('sectionToBeToggled2').classList.toggle('hide-extension')
+}
+if(id == 3){
+    document.getElementById('sectionToBeToggled3').classList.toggle('hide-extension')
+} 
+if(id == 4){
+    document.getElementById('sectionToBeToggled4').classList.toggle('hide-extension')
+} 
+if(id == 5){
+    document.getElementById('sectionToBeToggled5').classList.toggle('hide-extension')
+} 
+if(id == 6){
+    document.getElementById('sectionToBeToggled6').classList.toggle('hide-extension')
+} 
+if(id == 7){
+    document.getElementById('sectionToBeToggled7').classList.toggle('hide-extension')
+} 
+if(id == 8){
+    document.getElementById('sectionToBeToggled8').classList.toggle('hide-extension')
+} 
+}
+
+
+//************  Cart logic *********************/
+let itemsList = ['Mayan Avocado Salad', 'Beef Burrito', 'Pulled Pork Burrito Bowl', 'Aztec Chicken Salad', 'Mexican Beef Salad', 'Mayan Avocado Salat', 'Veggi Salad','Vegan Salad'];
 let shoppingList = [];
 
 
 function addToCart(id) {
     if (id == 1 && shoppingList.includes(itemsList[0]) != itemsList.includes(itemsList[0])) {
-        shoppingList.push(itemsList[0]);
-        console.log(shoppingList);
+        let item = itemsList[0];
+        shoppingList.push(item);
         updateCart();
     }
     if (id == 2 && shoppingList.includes(itemsList[1]) != itemsList.includes(itemsList[1])) {
-        shoppingList.push(itemsList[1]);
-        console.log(shoppingList);
+        let item = itemsList[1];
+        shoppingList.push(item);
         updateCart();
     }
     if (id == 3 && shoppingList.includes(itemsList[2]) != itemsList.includes(itemsList[2])) {
-        shoppingList.push(itemsList[2]);
-        console.log(shoppingList);
+        let item = itemsList[2];
+        shoppingList.push(item);
+        updateCart();
+    }
+    if (id == 4 && shoppingList.includes(itemsList[3]) != itemsList.includes(itemsList[3])) {
+        let item = itemsList[3];
+        shoppingList.push(item);
+        updateCart();
+    }
+    if (id == 5 && shoppingList.includes(itemsList[4]) != itemsList.includes(itemsList[4])) {
+        let item = itemsList[4];
+        shoppingList.push(item);
+        updateCart();
+    }
+    if (id == 6 && shoppingList.includes(itemsList[5]) != itemsList.includes(itemsList[5])) {
+        let item = itemsList[5];
+        shoppingList.push(item);
+        updateCart();
+    }
+    if (id == 7 && shoppingList.includes(itemsList[6]) != itemsList.includes(itemsList[6])) {
+        let item = itemsList[6];
+        shoppingList.push(item);
+        updateCart();
+    }
+    if (id == 8 && shoppingList.includes(itemsList[7]) != itemsList.includes(itemsList[7])) {
+        let item = itemsList[7];
+        shoppingList.push(item);
         updateCart();
     }
 }
