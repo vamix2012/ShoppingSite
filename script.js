11af96596353824ffe7fa7099c6dc1f8586489be
Lucian-Cristian Toma <vamix2012@gmail.com>
2020-11-11 21:39:50 +0100

    Optimized Javascript code

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+let items = [
+    { 'name': 'Mayan Avocado Salad', 'description': 'Fresh salad with mixed vegetables and lemon juiced avocado', 'variations': 'variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado', 'price': 6.99 },
+    { 'name': 'Beef Burrito', 'description': 'Grilled Tortilla with grilled Beef and mixed vegetables', 'variations': 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat', 'price': 7.99 },
+    { 'name': 'Pulled Pork Burrito Bowl', 'description': 'Grilled Beef, mixed vegetables and rice', 'variations': 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat', 'price': 7.49 },
+    { 'name': 'Aztec Chicken Salad', 'price': 6.15 },
+    { 'name': 'Mexican Beef Salad', 'price': 6.99 },
+    { 'name': 'Mayan Avocado Salat', 'price': 6.99 },
+    { 'name': 'Veggi Salad', 'price': 5.99 },
+    { 'name': 'Vegan Salad', 'price': 5.99 }
+];
+
+let shoppingList = [];
+let shoppingPriceList = [];
+
 function updateMenu() {
     updatePopulars();
     updateTexMex();
@@ -9,29 +23,9 @@ function toggleButtons(id) {
     document.getElementById(selector).classList.toggle('hide-extension');
 }
 
-
-//************  Cart logic *********************/
-let items = [
-    { 'name': 'Mayan Avocado Salad', 'description': 'Fresh salad with mixed vegetables and lemon juiced avocado', 'variations': 'variatons: Chipotle-tomato sauce, Habanero sauce, more avoacado', 'price': 6.99 },
-    { 'name': 'Beef Burrito', 'description': 'Grilled Tortilla with grilled Beef and mixed vegetables', 'variations': 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat', 'price': 7.99 },
-    { 'name': 'Pulled Pork Burrito Bowl', 'description': 'Grilled Beef, mixed vegetables and rice', 'variations': 'Variatons: Chipotle-tomato sauce, Habanero sauce, Avocado sauce, more meat', 'price': 7.49 },
-    { 'name': 'Aztec Chicken Salad', 'price': 6.15 },
-    { 'name': 'Mexican Beef Salad', 'price': 6.99 },
-    { 'name': 'Mayan Avocado Salat', 'price': 6.99 },
-    { 'name': 'Veggi Salad', 'price': 5.99 },
-    { 'name': 'Vegan Salad', 'price': 5.99 }
-
-];
-
-
-
-let shoppingList = [];
-let shoppingPriceList = [];
-
-
 function updateTexMex() {
     let productContainer = document.getElementById('TexMexElements');
-    for (let i = 3; i < 8; i += 1) {
+    for (let i = 3; i < items.legth; i++) {
         let extendBtn = 'toggleButtons(' + i;
         let addBtn = 'addToCart(' + i;
         let item = document.createElement('div');
@@ -40,34 +34,14 @@ function updateTexMex() {
         let variatons = items[i].variations;
         let itemPrice = items[i].price;
         let id = 'sectionToBeToggled' + i;
-        item.innerHTML = `<div class="dish-selection-container">
-    <div onclick="${extendBtn})">
-        <h3>${name}</h3>
-        <h5>${description}</h5>
-        <h6>${variatons}</h6>
-        <br>
-        <h1>${itemPrice} €</h1>
-    </div>
-    <div id="${id}" class="hide-extension button-layout">
-        <button onclick="${addBtn})" class="btn btn-primary">Add to Cart</button>
-    </div>
-</div>`;
+
+        item.innerHTML = generateHTML(extendBtn, name, description, variatons, itemPrice, id, addBtn);
         productContainer.appendChild(item);
     }
 }
 
-function updatePopulars() {
-    let productContainer = document.getElementById('popularElements');
-    for (let i = 0; i < 3; i += 1) {
-        let extendBtn = 'toggleButtons(' + i;
-        let addBtn = 'addToCart(' + i;
-        let item = document.createElement('div');
-        let name = items[i].name;
-        let description = items[i].description;
-        let variatons = items[i].variations;
-        let itemPrice = items[i].price;
-        let id = 'sectionToBeToggled' + i;
-        item.innerHTML = `<div class="dish-selection-container">
+function generateHTML(extendBtn, name, description, variatons, itemPrice, id, addBtn){
+    return  `<div class="dish-selection-container">
     <div onclick="${extendBtn})">
         <h3>${name}</h3>
         <h5>${description}</h5>
@@ -79,6 +53,20 @@ function updatePopulars() {
         <button onclick="${addBtn})" class="btn btn-primary">Add to Cart</button>
     </div>
 </div>`;
+}
+
+function updatePopulars() {
+    let productContainer = document.getElementById('popularElements');
+    for (let i = 0; i < 3; i++) {
+        let extendBtn = 'toggleButtons(' + i;
+        let addBtn = 'addToCart(' + i;
+        let item = document.createElement('div');
+        let name = items[i].name;
+        let description = items[i].description;
+        let variatons = items[i].variations;
+        let itemPrice = items[i].price;
+        let id = 'sectionToBeToggled' + i;
+        item.innerHTML = generateHTML(extendBtn, name, description, variatons, itemPrice, id, addBtn);
         productContainer.appendChild(item);
     }
 }
@@ -96,10 +84,9 @@ function addToCart(id) {
 }
 
 function updatePrice() {
-    function percentage(num, per)
-{
-  return (num/100)*per;
-}
+    function percentage(num, per) {
+        return (num / 100) * per;
+    }
     let shoppingTotal = shoppingPriceList.reduce((acc, curr) => {
         return acc + curr;
     }, 0);
@@ -139,7 +126,7 @@ function orderComplete() {
         updateCart();
         updatePrice();
         document.getElementById('complete').classList.remove('hide');
-    }else{
+    } else {
         document.getElementById('incomplete').classList.remove('hide');
     }
 
